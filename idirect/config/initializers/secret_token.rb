941e8af522e2ac71c950c090953daccde1a82f4e
yuvaraja <yuvaraja@kreatio.com>
2016-12-23 09:58:47 +0530

    test

---
+++ idirect/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Idirect::Application.config.secret_token = '64c0b1727c851261750f625d2b44d3e5a09303c137385ff0054a81c22f1910decacebf3bc0b46ccffcca87f59a5fdb990829557002596c4759cb1b6ca1432751'

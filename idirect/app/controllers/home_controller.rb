911703026fa4c810ec37df1527607da3e3327eea
yuvaraja <yuvaraja@kreatio.com>
2016-12-23 22:32:23 +0530

    changes

---
+++ idirect/app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  #caches_page home
+
+  def home
+    render :template => "home/home"
+end
+
+end
--- idirect/config/routes.rb
+++ idirect/config/routes.rb
@@ -45,7 +45,7 @@ Idirect::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+    match "/home/:path" => "home#home"
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
